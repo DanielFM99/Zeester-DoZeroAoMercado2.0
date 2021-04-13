6358c6bf9e8c2c5a0ec6cc37da7789542a972ee6
DanielFM99 <daniel_fm99@hotmail.com>
2021-04-13 17:19:02 -0300

    update1

---
+++ HackerRank/10 Days of JavaScript/Day3-TryCatchFinally.js
@@ -0,0 +1,15 @@
+/*
+ * Complete the reverseString function
+ * Use console.log() to print to stdout.
+ */
+function reverseString(s) {
+
+    try {
+        s = s.split("").reverse().join("");
+        console.log(s);
+    } catch (error) {
+        console.log(`${error.message}\n${s}`);
+    }
+
+}
+
