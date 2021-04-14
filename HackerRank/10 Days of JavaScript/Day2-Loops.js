6ba3ad09644fd7e50ea1d05f1043d93b1c487c0b
DanielFM99 <daniel_fm99@hotmail.com>
2021-04-14 01:20:05 -0300

    update4

--- HackerRank/10 Days of JavaScript/Day2-Loops.js
+++ HackerRank/10 Days of JavaScript/Day2-Loops.js
@@ -3,17 +3,16 @@
  * Print your output using 'console.log()'.
  */
 function vowelsAndConsonants(s) {
-    for (let i = 0; i < s.length; i++){
-        if (s[i] == "a" || s[i] == "e" || s[i] == "i" || s[i] == "o" || s[i] == "u"){
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] == "a" || s[i] == "e" || s[i] == "i" || s[i] == "o" || s[i] == "u") {
             console.log(s[i])
-            
+
         }
     }
 
-    for (let i = 0; i < s.length; i++){
-        if (!(s[i] == "a" || s[i] == "e" || s[i] == "i" || s[i] == "o" || s[i] == "u")){
+    for (let i = 0; i < s.length; i++) {
+        if (!(s[i] == "a" || s[i] == "e" || s[i] == "i" || s[i] == "o" || s[i] == "u")) {
             console.log(s[i])
         }
     }
 }
-
--- HackerRank/10 Days of JavaScript/Day7-RegularExpressions1.js
+++ HackerRank/10 Days of JavaScript/Day7-RegularExpressions1.js
@@ -3,7 +3,7 @@ function regexVar() {
      * Declare a RegExp object variable named 're'
      * It must match a string that starts and ends with the same vowel (i.e., {a, e, i, o, u})
      */
-    let re = new RegExp(/^(.).*\1$/)    
+    let re = new RegExp(/^(.).*\1$/)
     /*
      * Do not remove the return statement
      */
